58d9a225f34b2c6206ece335f03eb6ce49b298cf
Firefly4266 <gaines.johnp@gmail.com>
2017-09-03 18:26:59 -0700

    completed conditional exercises

--- SandBoxC/Program.cs
+++ SandBoxC/Program.cs
@@ -7,7 +7,32 @@ namespace SandBoxC
     {
         static void Main(string[] args)
         {
-
+            string input = Console.ReadLine();
+            
+            if (input.Equals("Jp"))
+            {
+                Console.WriteLine("What's up Jp!!!");
+            }
+            else if (input.Equals("Joe"))
+            {
+                Console.WriteLine("What's up Joe!!!");
+            }
+            else
+            {
+                Console.WriteLine("Who the hell are you??!!!");
+            }
+            switch (input)
+            {
+                case "Jp":
+                    Console.WriteLine("What's up Jp!!!");
+                    break;
+                case "Joe":
+                    Console.WriteLine("What's up Joe!!!");
+                    break;
+                default:
+                    Console.WriteLine("Who the hell are you??!!!");
+                    break;
+            }
         }
     }
 }
