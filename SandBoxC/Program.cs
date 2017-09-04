9064d6ef03a72b304f657002f49937d46e610ee9
Firefly4266 <gaines.johnp@gmail.com>
2017-09-03 17:03:26 -0700

    finished comparison exercises.

--- SandBoxC/Program.cs
+++ SandBoxC/Program.cs
@@ -7,7 +7,29 @@ namespace SandBoxC
     {
         static void Main(string[] args)
         {
+            string someText = "Here is some Text.";
+            string someOtherText = "Here is some text.";
 
+            //using the OrdinalIgnoreCase version of Ordinal makes comparison case insensitive.
+            bool isEqual = someText.Equals(someOtherText, StringComparison.OrdinalIgnoreCase);
+            string addedText = (someText + " " + someOtherText + " " + "here is some more text.");
+            string formattedString = String.Format("{0} {1} here is some more text.", someText, someOtherText);
+            Console.WriteLine(addedText);
+            Console.WriteLine(formattedString);
+
+            Console.WriteLine(someText.Length);
+            Console.WriteLine(someText[8]);
+
+            Console.WriteLine(someText.Substring(8, 4));
+
+            Console.WriteLine(someText.ToLower());
+            Console.WriteLine(someText.ToUpper());
+
+            string anotherText = string.Empty;
+            //this is what should be used instead of---  string anotherText = "";
+
+            string replacedText = someText.Replace("Text", "String");
+            Console.WriteLine(replacedText);
         }
     }
 }
