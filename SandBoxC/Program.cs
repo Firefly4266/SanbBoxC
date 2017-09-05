afc4cb590e6363fce02c17959894dccf2131dfe7
Firefly4266 <gaines.johnp@gmail.com>
2017-09-04 21:52:39 -0700

    Started try catch exercizes

--- SandBoxC/Program.cs
+++ SandBoxC/Program.cs
@@ -7,6 +7,23 @@ namespace SandBoxC
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string input = Console.ReadLine();
+                int convertedNumber;
+                // convert input to number
+                //int.TryParse(input, out convertedNumber);
+
+                bool isConvertedSuccessfully = int.TryParse(input, out convertedNumber);
+
+                if (!isConvertedSuccessfully)
+                {
+                    throw new Exception("Conversion was not successful.");
+                }
+            }catch (Exception ex) {
+                Console.WriteLine("There was an error {0}", ex.Message);
+            }
+            Console.WriteLine("The rest of my application is still running.");
         }
     }
 }
