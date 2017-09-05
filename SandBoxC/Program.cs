cbbe48582e7bb1ac44c118c8cb2c53411061473e
Firefly4266 <gaines.johnp@gmail.com>
2017-09-04 20:54:12 -0700

    Finished exception exercise.

--- SandBoxC/Program.cs
+++ SandBoxC/Program.cs
@@ -7,6 +7,17 @@ namespace SandBoxC
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
+            int convertedNumber;
+            // convert input to number
+            //int.TryParse(input, out convertedNumber);
+
+            bool isConvertedSuccessfully = int.TryParse(input, out convertedNumber);
+
+            if (!isConvertedSuccessfully)
+            {
+                throw new Exception("Conversion was not successful.");
+            }
         }
     }
 }
